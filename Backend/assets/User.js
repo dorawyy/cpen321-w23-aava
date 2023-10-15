4f9cccb106152bce9bdc82ece7f21513e120eb43
chen-vv <vchen03@student.ubc.ca>
2023-10-15 15:23:45 -0700

    Defined Player and User class

--- Backend/assets/User.js
+++ Backend/assets/User.js
@@ -1,6 +1,22 @@
 class User {
-    constructor() {
-    }
+  constructor(token, username, rank) {
+    /**
+     * The unique token that identifies this user.
+     * Persists across sessions.
+     */
+    this.token = token;
+
+    /**
+     * The display name of this user.
+     */
+    this.username = username;
+
+    /**
+     * Representation of user’s skill level based on the
+     * total amount of points won across all past games played.
+     */
+    this.rank = rank;
+  }
 }
 
 export default User;

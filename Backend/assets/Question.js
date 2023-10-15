5abc57bd269a191a947cb7b43c4ebd56bc26dbea
Ariel <arielvi2002@gmail.com>
2023-10-15 12:38:00 -0700

    Created Question and Settings objects

--- Backend/assets/Question.js
+++ Backend/assets/Question.js
@@ -1,5 +1,12 @@
+// Version: 1.0
+// Description: Class that represents a question.
+
 class Question {
-    constructor() {
+    constructor(question, correctAnswer, incorrectAnswers, difficulty) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswers = incorrectAnswers;
+        this.difficulty = difficulty;
     }
 }
 
--- Backend/assets/Settings.js
+++ Backend/assets/Settings.js
@@ -1,7 +1,17 @@
+// Version: 1.0
+// Description: Class that represents a room's settings. This class is used to store the settings of a room. 
+
 class Settings {
-    constructor() {
-       
+    constructor(isPublic, questionCategories, questionDifficulties, maxPlayers, questionTime, numberQuestions) {
+        this.roomIsPublic = isPublic
+        this.questionCategories = questionCategories
+        this.questionDifficulties = questionDifficulties
+        this.maxPlayers = maxPlayers
+        this.questionTime = questionTime
+        this.numberQuestions = numberQuestions
     }
+
+    
 }
 
 export default Settings;

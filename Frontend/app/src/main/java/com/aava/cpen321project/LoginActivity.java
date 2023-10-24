09f6b0909081baca53d5c03127b2d9d6472374e1
AprilJu <97860422+AprilJu@users.noreply.github.com>
2023-10-23 17:22:38 -0700

    Commit to save progress before switching to development branch

--- Frontend/app/src/main/java/com/aava/cpen321project/LoginActivity.java
+++ Frontend/app/src/main/java/com/aava/cpen321project/LoginActivity.java
@@ -6,24 +6,113 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class LoginActivity extends Activity {
+    private GoogleSignInClient mGoogleSignInClient;
+    private Integer RC_SIGN_IN = 1;
+    final static String TAG = "MenuActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login); // Replace with your login layout name
 
-        Button loginButton = findViewById(R.id.signin_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
-                startActivity(intent);
+                // Configure sign-in to request the user's ID, email address, and basic
+                // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+
+                mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+                findViewById(R.id.signin_button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        signIn();
+                    }
+                });
+
+        }
+
+
+    private void signIn() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //TODO
+            ///updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            // Check for existing Google Sign In account, if the user is already signed in
+            // the GoogleSignInAccount will be non-null.
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            updateUI(account);
+        }
+
+
+        private void updateUI(GoogleSignInAccount account) {
+            if(account == null){
+                Log.d(TAG, "no users signed in");
             }
-        });
+            else{
+                Log.d(TAG, "Pref Name: " + account.getDisplayName());
+                Log.d(TAG, "Email: " + account.getEmail());
+                Log.d(TAG, "Display URL: " + account.getPhotoUrl());
+                Log.d(TAG, "Given Name: " + account.getGivenName());
+
+                //Send the token to back-end
+                //account.getIdToken();
+                //Move to another activity
+                final String userN = account.getGivenName() + account.getFamilyName();
+
+                Intent serverIntent = new Intent(MainActivity.this, ServerInfoActivity.class);
+                serverIntent.putExtra("KEY_STRING", userN);
+                startActivity(serverIntent);
+
+
+            }
+        }
     }
 }
 
--- Frontend/app/src/main/java/com/aava/cpen321project/MenuActivity.java
+++ Frontend/app/src/main/java/com/aava/cpen321project/MenuActivity.java
@@ -2,13 +2,62 @@ package com.aava.cpen321project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private TextView titleText, playText, accountText, userText;
+    private ImageButton playButton, createButton, codeButton1, codeButton2;
+
+    private ImageView logoImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        // Initialize views
+        titleText = findViewById(R.id.titleText);
+        playText = findViewById(R.id.playText);
+        accountText = findViewById(R.id.accountText);
+        userText = findViewById(R.id.userText);
+
+        playButton = findViewById(R.id.playButton);
+        createButton = findViewById(R.id.createButton);
+        codeButton1 = findViewById(R.id.codeButton1);
+        codeButton2 = findViewById(R.id.codeButton2);
+        logoImage = findViewById(R.id.logoImage);
+
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.playButton:
+                        // Do something for button1
+                        Intent intent = new Intent(MenuActivity.this, LobbyActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.createButton:
+                        // Do something for button2
+                        break;
+                    case R.id.codeButton1:
+                        // Do something for button3
+                        break;
+                    case R.id.codeButton2:
+                        // Do something for button3
+                        break;
+                }
+            }
+        };
+
+        playButton.setOnClickListener(buttonClickListener);
+        createButton.setOnClickListener(buttonClickListener);
+        codeButton1.setOnClickListener(buttonClickListener);
+        codeButton2.setOnClickListener(buttonClickListener);
     }
 }

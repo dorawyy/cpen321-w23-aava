4d2150329ff80aa1a2b29e48143ef489ead76072
AprilJu <97860422+AprilJu@users.noreply.github.com>
2023-10-25 14:19:54 -0700

    upd login/menu
    Get back work being reverted

--- Frontend/app/src/main/java/com/aava/cpen321project/LoginActivity.java
+++ Frontend/app/src/main/java/com/aava/cpen321project/LoginActivity.java
@@ -2,9 +2,29 @@ package com.aava.cpen321project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.Manifest;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private Integer RC_SIGN_IN = 1;
+    final static String TAG = "MenuActivity";
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

faea2d787828ef2f5bd3810f98bdd177d5f9d101
Aledax <dalexay16@gmail.com>
2023-10-25 13:48:44 -0700

    Revert "upd login&menu layout"
    This reverts commit 75b6f49e3e3f5d985d7c4b8b2be275d3280266e7.

--- Frontend/app/src/main/java/com/aava/cpen321project/LoginActivity.java
+++ Frontend/app/src/main/java/com/aava/cpen321project/LoginActivity.java
@@ -3,28 +3,12 @@ package com.aava.cpen321project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); // Replace with your login layout name
-
-        Button loginButton = findViewById(R.id.signin_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_login);
     }
 }
-
-
